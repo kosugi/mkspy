cd942f6432bf1b393d3b902e3125700791e7cf9b
KOSUGI Tomo <kosugi.tomo@gmail.com>
2024-02-07 03:28:23 +0900

    use subsystem “windows”

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use iced::widget::Text;
 use iced::{executor, time, window, Application, Command, Element, Font, Settings, Theme};
 
