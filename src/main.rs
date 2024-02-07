d1cf736e7a6f14ce5783ddbf80f7294048ada425
KOSUGI Tomo <kosugi.tomo@gmail.com>
2024-02-08 06:13:29 +0900

    Win32 APIize

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use iced::widget::Text;
-use iced::{executor, time, window, Application, Command, Element, Font, Settings, Theme};
-
 use windows::Win32::UI::Input::KeyboardAndMouse::{
     GetAsyncKeyState, VIRTUAL_KEY, VK_CONTROL, VK_LWIN, VK_MENU, VK_RWIN, VK_SHIFT,
+};
+
+use windows::core::{w, Result, HSTRING, PCWSTR};
+use windows::Win32::{
+    Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
+    Graphics::Gdi::{
+        BeginPaint, CreateFontW, DeleteObject, EndPaint, InvalidateRect, SelectObject, TextOutW,
+        UpdateWindow, HFONT, PAINTSTRUCT,
+    },
+    System::LibraryLoader::GetModuleHandleW,
+    UI::WindowsAndMessaging::{
+        CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, PostQuitMessage,
+        RegisterClassW, SetTimer, ShowWindow, CW_USEDEFAULT, HMENU, MSG, SW_SHOW, WM_CREATE,
+        WM_DESTROY, WM_PAINT, WM_TIMER, WNDCLASSW, WS_EX_TOPMOST, WS_OVERLAPPEDWINDOW,
+    },
 };
 
 fn choose<T>(condition: bool, lhs: T, rhs: T) -> T {
@@ -35,80 +47,103 @@ fn is_win_pressed() -> bool {
     is_key_pressed(VK_LWIN) || is_key_pressed(VK_RWIN)
 }
 
-struct MyApp {
-    is_shift_pressed: bool,
-    is_ctrl_pressed: bool,
-    is_alt_pressed: bool,
-    is_win_pressed: bool,
-}
+unsafe extern "system" fn window_proc(
+    hwnd: HWND,
+    message: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    static mut HFONT: Option<HFONT> = None;
 
-impl MyApp {
-    fn default() -> Self {
-        Self {
-            is_shift_pressed: false,
-            is_ctrl_pressed: false,
-            is_alt_pressed: false,
-            is_win_pressed: false,
+    match message {
+        WM_CREATE => {
+            HFONT = Some(CreateFontW(
+                16,
+                0,
+                0,
+                0,
+                400,
+                false.into(),
+                false.into(),
+                false.into(),
+                1,
+                0,
+                0,
+                0,
+                0,
+                w!("Courier New"),
+            ));
+            LRESULT(0)
         }
-    }
-}
+        WM_TIMER => {
+            InvalidateRect(hwnd, None, true);
+            LRESULT(0)
+        }
+        WM_PAINT => {
+            let s = format!(
+                "[{}][{}][{}][{}]",
+                choose(is_shift_pressed(), "SHIFT", "     "),
+                choose(is_ctrl_pressed(), "CTRL", "    "),
+                choose(is_alt_pressed(), "ALT", "   "),
+                choose(is_win_pressed(), "WIN", "   ")
+            );
 
-#[derive(Debug, Clone)]
-enum Message {
-    Tick(chrono::DateTime<chrono::Utc>),
-}
-
-impl Application for MyApp {
-    type Executor = executor::Default;
-    type Message = Message;
-    type Theme = Theme;
-    type Flags = ();
-
-    fn new(_flags: ()) -> (MyApp, Command<Message>) {
-        (MyApp::default(), Command::none())
-    }
-
-    fn title(&self) -> String {
-        String::from("Modifier Key Spy")
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::Tick(_local_time) => {
-                self.is_shift_pressed = is_shift_pressed();
-                self.is_ctrl_pressed = is_ctrl_pressed();
-                self.is_alt_pressed = is_alt_pressed();
-                self.is_win_pressed = is_win_pressed();
+            let mut ps = PAINTSTRUCT::default();
+            let hdc = BeginPaint(hwnd, &mut ps);
+            if let Some(hfont) = HFONT {
+                SelectObject(hdc, hfont);
             }
+            TextOutW(hdc, 0, 0, HSTRING::from(&s).as_wide());
+            EndPaint(hwnd, &ps);
+            LRESULT(0)
         }
-        Command::none()
-    }
-
-    fn view(&self) -> Element<Message> {
-        let s = format!(
-            "[{}][{}][{}][{}]",
-            choose(self.is_shift_pressed, "SHIFT", "     "),
-            choose(self.is_ctrl_pressed, "CTRL", "    "),
-            choose(self.is_alt_pressed, "ALT", "   "),
-            choose(self.is_win_pressed, "WIN", "   "),
-        );
-        Text::new(s).font(Font::MONOSPACE).size(16).into()
-    }
-
-    fn subscription(&self) -> iced::Subscription<Self::Message> {
-        time::every(std::time::Duration::from_millis(100))
-            .map(|_| Message::Tick(chrono::Utc::now()))
+        WM_DESTROY => {
+            if let Some(hfont) = HFONT.take() {
+                DeleteObject(hfont);
+            }
+            PostQuitMessage(0);
+            LRESULT(0)
+        }
+        _ => DefWindowProcW(hwnd, message, wparam, lparam),
     }
 }
 
-fn main() -> iced::Result {
-    MyApp::run(Settings {
-        window: window::Settings {
-            resizable: false,
-            size: (190, 25),
-            level: window::Level::AlwaysOnTop,
-            ..window::Settings::default()
-        },
-        ..Settings::default()
-    })
+fn main() -> Result<()> {
+    unsafe {
+        let Ok(instance) = GetModuleHandleW(PCWSTR::null()) else {
+            return Ok(());
+        };
+        let window_class = w!("ModifierKeySpy");
+        let wc = WNDCLASSW {
+            hInstance: HINSTANCE(instance.0),
+            lpszClassName: window_class,
+            lpfnWndProc: Some(window_proc),
+            ..Default::default()
+        };
+        RegisterClassW(&wc);
+
+        let hwnd = CreateWindowExW(
+            WS_EX_TOPMOST,
+            window_class,
+            w!("Modifier Key Spy"),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            200,
+            60,
+            HWND(0),
+            HMENU(0),
+            instance,
+            None,
+        );
+        ShowWindow(hwnd, SW_SHOW);
+        UpdateWindow(hwnd);
+        SetTimer(hwnd, 1, 100, None);
+
+        let mut msg = MSG::default();
+        while GetMessageW(&mut msg, HWND(0), 0, 0).into() {
+            DispatchMessageW(&msg);
+        }
+    }
+    Ok(())
 }
